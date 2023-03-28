d6d5dfb6ba71132d14b894e6084ee266b3b01bdc
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2023-03-28 16:53:02 +0700

    выражения и операции в java

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,28 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {                        //функция/метод (именованный блок кода с параметрами ф-и () для последующего обращения)
         System.out.println("Hello,world");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+
+        System.out.println(1 / 2);                                    //в операции целые числа, в результате тож целые числа
+        System.out.println(1.0 / 2);                                  //в операции дробные числа, в результате тож дробные числа(с плавающей точкой)
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");                                    //конкатенация - склеивание строк (только +)
+        System.out.println("2" + 2);                                        //число преобразовывается в строку
+
+        System.out.println(2 + 2 * 2);
+        System.out.println( (2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + 2 + 2 );                        //выполнена конкатенация, чтобы числа считались, надо заключить их в скобки
+        System.out.println("2 + 2 = " + (2 + 2) );
+
+
     }
 
 }
