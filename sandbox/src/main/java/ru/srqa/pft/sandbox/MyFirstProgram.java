7fe26af5fb4f7fa2a763eb8eb92aed228b9af639
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2023-03-28 15:02:17 +0700

    Класс MyFirstProgram - перемещен в пакет (вложенные директории - для избегания  конфликтов с наименованием)

---
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,9 @@
+package ru.srqa.pft.sandbox;        //указание,что класс MyFirstProgram лежит в пакете (архитектура вложенных директорий)
+
+public class MyFirstProgram {
+
+public static void main(String[] args) {
+System.out.println("Hello,world");
+}
+
+}
